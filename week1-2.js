08ad45f28186049fe6fd8e08c1db2c90538eb242
Srujana-Byrraju <srujanabyrraju334@gmail.com>
2026-08-02 23:21:08 +0530

    Week 1 Experiment 2

--- week1-2.js
+++ week1-2.js
@@ -1,20 +1,15 @@
+"use strict";
 // Week 1 - Experiment 2
 // Special Types: any, unknown, void
-
-let data: any = "Full Stack";
+let data = "Full Stack";
 console.log("Any Type:", data);
-
 data = 2026;
 console.log("Updated Any Type:", data);
-
-let value: unknown = "Srujana";
-
+let value = "Srujana";
 if (typeof value === "string") {
     console.log("Unknown Type:", value.toUpperCase());
 }
-
-function showMessage(message: string): void {
+function showMessage(message) {
     console.log("Message:", message);
 }
-
 showMessage("Welcome to TypeScript");
