0b2714e36f57b649f9ac1d196d6220ec5777bf68
Srujana-Byrraju <srujanabyrraju334@gmail.com>
2026-08-17 09:02:42 +0530

    Delete week4-1Main.js

--- week4-1Main.js
+++
@@ -1,9 +0,0 @@
-// main.ts
-import { Order } from "./week4-1OrderLogic.js";
-const customer = {
-    customerName: "Srujana",
-    age: 19,
-    address: "Bhimavaram"
-};
-const order = new Order(customer, 500, 101);
-order.printBill();
